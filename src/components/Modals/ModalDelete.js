84a2029f4c12b346220556b0e1829086e0dff460
alvindioprakosa <alvindioprakosa@gmail.com>
2025-06-07 10:38:21 +0700

    Update Modal Delete

--- src/components/Modals/ModalDelete.js
+++ src/components/Modals/ModalDelete.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
+import PropTypes from 'prop-types';
 import { Modal, Spinner } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import alertIcon from "../../assets/images/icon-alert.svg";
@@ -8,18 +9,42 @@ function ModalDelete({ show, handleClose, title, text, deletedItem, handleDelete
   const dispatch = useDispatch();
 
   const { 
-    isLoadingDeleteActivity, dataDeleteActivity, errDeleteActivity,
-    isLoadingDeleteItem, dataDeleteItem, errDeleteItem
+    isLoadingDeleteActivity, 
+    dataDeleteActivity, 
+    errDeleteActivity,
+    isLoadingDeleteItem, 
+    dataDeleteItem, 
+    errDeleteItem
   } = useSelector((state) => state.todo);
+
+  const resetState = useCallback(
+    () => dispatch(TodoActions.resetStateTodo()),
+    [dispatch]
+  );
 
   useEffect(() => {
     if (errDeleteActivity !== null || dataDeleteActivity || errDeleteItem !== null || dataDeleteItem) {
       handleClose();
-      dispatch(TodoActions.resetStateTodo());
+      resetState();
     }
-  }, [errDeleteActivity, dataDeleteActivity, errDeleteItem, dataDeleteItem, handleClose, dispatch]);
+  }, [
+    errDeleteActivity, 
+    dataDeleteActivity, 
+    errDeleteItem, 
+    dataDeleteItem, 
+    handleClose, 
+    resetState
+  ]);
 
-  const handleClickDelete = () => handleDelete ? handleDelete() : dispatch(TodoActions.deleteActivityRequest(deletedItem));
+  const handleClickDelete = useCallback(() => {
+    if (handleDelete) {
+      handleDelete();
+    } else {
+      dispatch(TodoActions.deleteActivityRequest(deletedItem));
+    }
+  }, [handleDelete, deletedItem, dispatch]);
+
+  const isLoading = isLoadingDeleteActivity || isLoadingDeleteItem;
 
   return (
     <Modal
@@ -33,15 +58,28 @@ function ModalDelete({ show, handleClose, title, text, deletedItem, handleDelete
     >
       <Modal.Header>
         <Modal.Title className="pt-4">
-          <img src={alertIcon} alt="alert" data-cy="modal-delete-icon" />
-          <h4 className="font-weight-bold" data-cy="modal-delete-title">{title}</h4>
+          <img 
+            src={alertIcon} 
+            alt="alert" 
+            data-cy="modal-delete-icon" 
+          />
+          <h4 className="font-weight-bold" data-cy="modal-delete-title">
+            {title}
+          </h4>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p className="px-3" dangerouslySetInnerHTML={{ __html: text }}></p>
+        <p 
+          className="px-3" 
+          dangerouslySetInnerHTML={{ __html: text }}
+        />
       </Modal.Body>
       <Modal.Footer className="pb-4">
-        <button className="btn btn-secondary" data-cy="modal-delete-cancel-button" onClick={handleClose}>
+        <button 
+          className="btn btn-secondary" 
+          data-cy="modal-delete-cancel-button" 
+          onClick={handleClose}
+        >
           Batal
         </button>
         <button 
@@ -49,12 +87,29 @@ function ModalDelete({ show, handleClose, title, text, deletedItem, handleDelete
           data-cy="modal-delete-confirm-button" 
           onClick={handleClickDelete} 
           aria-live="polite"
+          disabled={isLoading}
         >
-          {(isLoadingDeleteActivity || isLoadingDeleteItem) ? <Spinner animation="border" size="sm" /> : "Hapus"}
+          {isLoading ? (
+            <Spinner animation="border" size="sm" />
+          ) : (
+            "Hapus"
+          )}
         </button>
       </Modal.Footer>
     </Modal>
   );
 }
 
-export default ModalDelete;
+ModalDelete.propTypes = {
+  show: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  deletedItem: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  handleDelete: PropTypes.func
+};
+
+export default React.memo(ModalDelete);
